7a1ea0e53e913c89ba543be67ce7ca708acecc25
nadia.co <ngarciaorozco@outlook.com>
2020-09-06 01:02:57 -0600

    prototipo de portafolio de Nadia listo

--- portafolios/PortafolioNadia/main.js
+++ portafolios/PortafolioNadia/main.js
@@ -1,8 +1,9 @@
-window.onscroll = function() {scrollFunction()};
+
+/*window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.getElementById("nav").style.backgroundColor = "red";
   } else {
   }
-}
+}*/
