2dccdf0c4c86c9e501668a69acc2cd6ad648ca52
nadia.co <ngarciaorozco@outlook.com>
2020-09-05 20:08:08 -0600

    Header Portafolio Nadia

---
+++ portafolios/PortafolioNadia/main.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("nav").style.backgroundColor = "red";
+  } else {
+  }
+}
